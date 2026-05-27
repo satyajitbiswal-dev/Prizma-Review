3ceadc33ceba17352ba0febba237264fb30399e2
Satyajit Biswal <biswalsatyajit787@gmail.com>
2026-05-27 19:08:16 +0530

    add the initial architecture of the project

---
+++ config/settings/__init__.py
@@ -0,0 +1,5 @@
+from .base import *
+
+# Optionally allow selecting different settings modules via an
+# environment variable in the future, e.g. DJANGO_SETTINGS_MODULE
+# can reference config.settings.local or config.settings.production.
